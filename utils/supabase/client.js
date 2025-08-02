fe1409e7c41e6c55a91e4160cc1dcc596785c8f4
RyanSantosRibeiro <santosryan131@gmail.com>
2025-08-02 12:01:25 -0300

    fix: alpha 6

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -1,17 +1,16 @@
 import { createBrowserClient } from '@supabase/ssr';
-import { Database } from '@/types_db';
 
 // Define a function to create a Supabase client for client-side operations
 export const createClient = () =>
   createBrowserClient<Database>(
     // Pass Supabase URL and anonymous key from the environment to the client
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 
 export function createClientAdmin() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY,
   );
 }
